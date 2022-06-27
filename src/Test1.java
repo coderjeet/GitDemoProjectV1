603c57893ab962a1bddf587ceedec940b03f3642
coderjeet <developer.prasanjeet@gmail.com>
2022-06-28 01:05:04 +0530

    First commit...

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public Test1() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		int a =20;
+		int b =30;
+		
+		int result = (a>b)?a:b;
+		System.out.println(result);
+	}
+
+}
